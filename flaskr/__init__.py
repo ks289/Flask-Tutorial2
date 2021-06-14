a4dfb7ca76080d8ab8d464ad6d47b32137d2b484
ks289 <ks289@st-andrews.ac.uk>
2021-06-14 13:09:43 +0100

    setup

---
+++ flaskr/__init__.py
@@ -0,0 +1,31 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    # creating and configuring the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+    
+    if test_config is None:
+        # loading instance config if it exists
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # loading the test config if specified
+        app.config.from_mapping(test_config)
+        
+    # ensuring the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    # simple page for now
+    @app.route('/hello')
+    def hello():
+        return 'Hello'
+    
+    return app
---
+++ readme
@@ -0,0 +1,9 @@
+To launch the venv:
+
+source env/bin/activate
+
+To run:
+
+export FLASK_APP=flaskr
+export FLASK_ENV=development
+flask run
